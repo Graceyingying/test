e7951b43e0c79970a25e9caf600645324679b183
Huang Ying Ying <grace03huang@gmail.com>
2026-02-24 05:48:07 +0000

    5 break customer model down to multiple models

--- models/customers.sql
+++ models/customers.sql
@@ -1,25 +1,4 @@
-with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
-
-),
-
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
-
-),
+with
 
 customer_orders as (
 
@@ -53,3 +32,7 @@ final as (
 )
 
 select * from final
+
+
+select * from {{ ref('stg_customers') }}
+select * from {{ ref('stg_orders') }}
